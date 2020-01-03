dc7d5169642637e33e061faa04c32708dd498bc9
mateuscumerlatto <mateuscumerlatto@gmail.com>
2020-01-03 17:43:23 -0300

    typo

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ echo "Symlinks:"
 
 ln -s $(pwd)/git/.gitconfig ~/.gitconfig
 
-echo "Homebrew"
+echo "Homebrew:"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
