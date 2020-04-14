e30bca278d7b64deb4879e54eecfbbf983736d2c
mateuscumerlatto <mateuscumerlatto@gmail.com>
2020-04-13 23:01:12 -0300

    add gitignore file and install

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 echo "Symlinks:"
 
 ln -s $(pwd)/git/.gitconfig ~/.gitconfig
+ln -s $(pwd)/git/.gitignore_global ~/.gitignore_global
 
 echo "Homebrew:"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Git Ignore:"
+git config --global core.excludesfile ~/.gitignore_global
