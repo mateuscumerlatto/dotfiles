320b86380a849f204746456027c5d6fbca5ee1a2
Mateus Cumerlatto <mateus.cumerlatto@contaazul.com>
2020-04-13 23:03:12 -0300

    typo

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ ln -s $(pwd)/git/.gitignore_global ~/.gitignore_global
 echo "Homebrew:"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "Git Ignore:"
+echo "Git:"
 git config --global core.excludesfile ~/.gitignore_global
