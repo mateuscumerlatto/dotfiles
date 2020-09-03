b51fdc75b50337391ee3d5ae174b852f5a88c4dd
mateuscumerlatto <mateuscumerlatto@gmail.com>
2020-09-03 00:53:39 -0300

    add new files and commands

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# [WIP] 
-
-## dotfiles
+# dotfiles
 My dotfiles
+
+## install
+```
+$ git clone https://github.com/mateuscumerlatto/dotfiles.git ~/.dotfiles
+$ cd ~/.dotfiles
+$ chmod +x install.sh
+$ ./install.sh
+```
--- install.sh
+++ install.sh
@@ -13,14 +13,24 @@ brew cask install google-chrome
 echo "VS Code"
 brew cask install visual-studio-code
 
+echo "Sublime Text"
+brew cask install sublime-text
+
+echo "iTerm"
+brew cask install iterm2
+
 echo "Github CLI"
 brew install github/gh/gh
 
-echo "Git"
+echo "Configs"
+
+echo "Git config"
 git config --global core.excludesfile ~/.gitignore_global
 
 echo "zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 echo "creating Code folder"
 mkdir ~/Code
