077ad1f5a1d2c3995eb0a61c90b16597c16e0700
mateuscumerlatto <mateuscumerlatto@gmail.com>
2020-09-09 00:06:59 -0300

    fixing install and remove alias files

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ echo "Symlinks:"
 ln -s $(pwd)/git/.gitconfig ~/.gitconfig
 ln -s $(pwd)/git/.gitignore_global ~/.gitignore_global
 ln -s $(pwd)/editorconfig/.editorconfig ~/.editorconfig
+ln -s $(pwd)/zsh/.zshrc ~/.zshrc
+
 
 echo "Settings:"
 
@@ -11,11 +13,13 @@ git config --global core.excludesfile ~/.gitignore_global
 
 echo "zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+source ~/.zshrc
 
-source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+echo "Zsh default shell"
+chsh -s $(which zsh)
 
 
-echo "Creating folders..."
+echo "Folders:"
 
 echo "Code folder"
 mkdir ~/Code
