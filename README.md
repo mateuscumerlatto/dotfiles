88af288e7722278cb637ef36c60a411f34e02934
mateuscumerlatto <mateuscumerlatto@gmail.com>
2020-09-08 22:27:54 -0300

    removing brew installs

--- README.md
+++ README.md
@@ -1,27 +1,10 @@
 # dotfiles
 My dotfiles
 
-## install
+## Install
 ```
 $ git clone https://github.com/mateuscumerlatto/dotfiles.git ~/.dotfiles
 $ cd ~/.dotfiles
 $ chmod +x install.sh
 $ ./install.sh
 ```
-
-## machine setup checklist
-
-- [ ] SSH Keys (id_rsa and id_rsa.pub);
-- [ ] 1Password;
-- [ ] Authy;
-- [ ] Github Sign (gpg);
-- [ ] iTerm;
-- [ ] Docker;
-- [ ] Chrome;
-- [ ] Spotify;
-- [ ] WhatsApp;
-- [ ] Slack;
-- [ ] DBeaver;
-- [ ] PyCharm CE;
-- [ ] VSCode;
-- [ ] Sublime Text;
--- install.sh
+++ install.sh
@@ -4,30 +4,7 @@ ln -s $(pwd)/git/.gitconfig ~/.gitconfig
 ln -s $(pwd)/git/.gitignore_global ~/.gitignore_global
 ln -s $(pwd)/editorconfig/.editorconfig ~/.editorconfig
 
-
-echo "Homebrew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-echo "Chrome"
-brew cask install google-chrome
-
-echo "VS Code"
-brew cask install visual-studio-code
-
-echo "Sublime Text"
-brew cask install sublime-text
-
-echo "iTerm"
-brew cask install iterm2
-
-echo "Github CLI"
-brew cask install github/gh/gh
-
-echo "gnupg"
-brew cask install gnupg
-
-
-echo "Settings..."
+echo "Settings:"
 
 echo "Git config"
 git config --global core.excludesfile ~/.gitignore_global
